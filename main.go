f4cd4922b166e7e4b23a88d32ed2aea0cfb610c3
Ashikur Rahaman <nishuthetomlover@gmail.com>
2024-04-01 18:32:13 +0600

    Create main.go

---
+++ main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"fmt"
+)
+
+func main() {
+	readme := `
+# SPUDBLOCKS'S FINANCE TRACKER
+
+## Track Your Daily Finances Through Telegram
+
+Welcome to SPUDBLOCKS'S Finance Tracker, your ultimate companion for managing your daily expenses conveniently through Telegram! This tool is designed to simplify the process of monitoring your finances, ensuring that you stay in control of your expenses no matter where you are.
+
+### Exciting Features
+
+- **Expense Logging**: Easily log your expenses directly within Telegram using simple commands.
+- **Debt Tracking**: Keep track of any debts you owe by logging transactions with the /Debt command.
+- **Credit Recording**: Record credit transactions effortlessly with the /Credit command.
+- **Expense Analysis**: Gain insights into your spending habits with commands like /Last7, /Today, and /Last30, which allow you to review your expenses over different time periods.
+- **User-Friendly Interface**: With a seamless integration into Telegram, accessing and managing your financial data is just a few taps away.
+
+### How It Works
+
+1. **Log Your Expenses**: Use commands like /Debt and /Credit followed by the amount and a brief description to log your expenses.
+
+   Example:
+   /Debt 10 Bus Cost
+   /Credit 20 Dad Gift
+
+2. **Review Your Expenses**: Utilize commands such as /Last7, /Today, and /Last30 to review your expenses over specific time periods.
+
+3. **Stay In Control**: With real-time access to your financial data, you can stay on top of your expenses and make informed decisions about your finances.
+
+### Getting Started
+
+To start tracking your expenses with SPUDBLOCKS'S Finance Tracker, simply add the bot to your Telegram by searching for @SPUDFNCBOT. Once added, you can begin logging your expenses and gaining insights into your spending habits right away!
+
+Whether you're a seasoned budgeting pro or just starting to take control of your finances, SPUDBLOCKS'S Finance Tracker is here to make managing your money easier and more efficient than ever before. Start tracking your expenses effortlessly today!
+
+🔗 [Add SPUDBLOCKS'S Finance Tracker to Telegram](https://t.me/SPUDFNCBOT)
+`
+	fmt.Println(readme)
+}
